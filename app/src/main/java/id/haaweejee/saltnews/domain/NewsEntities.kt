f5c21e4c47a51098bd1243ebe1dd0caa4c717d7e
Hendrawan Wibowo Jatmiko <52823787+haaweejee@users.noreply.github.com>
2024-02-28 14:27:00 +0700

    + create entities

---
+++ app/src/main/java/id/haaweejee/saltnews/domain/NewsEntities.kt
@@ -0,0 +1,10 @@
+package id.haaweejee.saltnews.domain
+
+data class NewsEntities(
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+    val publishedDate: String,
+    val newsUrl: String,
+    val newsSource: String,
+)
